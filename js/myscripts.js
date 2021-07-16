0e2799a1eb32e2e359d1847723408b54e0c5bc9c
naruta1105 <dien.tranxuan110594@gmail.com>
2021-07-16 09:37:46 +0700

    Fix slideshow in home page
    issue is 2nd Slideshow in homepage show 1st item -> change myscript to fix

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,12 +1,28 @@
-let items = document.querySelectorAll('.carousel .carousel-item')
+let items01 = document.querySelectorAll('#carousel01 .carousel-item')
 
-items.forEach((el) => {
-    const minPerSlide = 5
+items01.forEach((el) => {
+    const minPerSlide = 4 
     let next = el.nextElementSibling
     for (var i=1; i<minPerSlide; i++) {
         if (!next) {
             // wrap carousel by using first child
-        	next = items[0]
+        	next = items01[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+let items02 = document.querySelectorAll('#carousel02 .carousel-item')
+
+items02.forEach((el) => {
+    const minPerSlide = 4 
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items02[0]
       	}
         let cloneChild = next.cloneNode(true)
         el.appendChild(cloneChild.children[0])
